0ba5eebcc87cbcbc1eee4a8b2a8b03de4e63a0a0
Philipp Fehr <calvin.philipp@gmail.com>
2018-03-01 19:31:33 +0100

    Adding parsing functionalities

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const { FileIO } = require('./FileIO');
 const process = require('process');
+const Parser = require('./parser');
 
 module.exports = {
   async main() {
     try {
-      let data = await new FileIO('data/example.in').read();
+      let data = await new FileIO('data/a_example.in').read();
       console.log(data);
+      let models = Parser.parseData(data);
+      console.log(models);
 
       await new FileIO('output/example.out').write('test');
     } catch(err) {
--- src/parser/index.js
+++ src/parser/index.js
@@ -1,3 +1,44 @@
+const Simulation = require('../models/Simulation.js');
+const Vehicle = require('../models/Vehicle.js');
+const Ride = require('../models/Ride.js');
+
 module.exports = class Parser {
+
+    static parseData(rawInput) {
+        let sim = this.parseFirsLine(rawInput.split('\n').shift())
+
+        let lines = rawInput.trim().split('\n');
+        let rs = [];
+        let vs = [];
+
+        lines.forEach((val, id) => {
+            let parts = val.split(' ');
+            let ride = new Ride(id, parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+
+            rs.push(ride);
+        });
+
+        for (var i = 0; i < sim.vehicleCount; i++)
+        {
+            let v = new Vehicle(i);
+            vs.push(v);
+        }
+
+        return {
+            simulation: sim,
+            rides: rs,
+            vehicles: vs
+        }
+    }
+
+    static parseFirsLine(firstLine) {
+        let parts = firstLine.split(' ');
+
+        let simulation = new Simulation(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+
+        return simulation;
+    }
+
+
 
 };
