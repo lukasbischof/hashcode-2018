1d3fa9f6fb15ab08a729e3cf16e4840ee5280a6b
Philipp Fehr <calvin.philipp@gmail.com>
2018-03-01 20:59:39 +0100

    puuuush

--- src/models/Ride.js
+++ src/models/Ride.js
@@ -24,6 +24,12 @@ module.exports = class Ride {
     return Math.abs(this.endPositionX - this.startPositionX) + Math.abs(this.endPositionY - this.startPositionY);
   }
 
+  get scoring(vehicle, currentSimulationStep, timeBonus) {
+    let steps = this.steps();
+    let timeB = timeBonus;
+    // let 
+  }
+
   distanceToMe(myX, myY) {
     return Math.abs(myX - this.startPositionX) + Math.abs(myY - this.startPositionY);
   }
